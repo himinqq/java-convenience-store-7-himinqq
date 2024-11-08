e52d49001383f3a76ff1c6447795296ef3c86610
himinqq <alsrud0966@naver.com>
2024-11-08 17:37:36 +0900

    test(ProductTest): 구매 수량만큼 재고에서 차감되는 기능 테스트

---
+++ src/test/java/store/domain/ProductsTest.java
@@ -0,0 +1,20 @@
+package store.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ProductsTest {
+    @Test
+    void 고객이구매한_수량만큼_재고에서_차감된다() {
+        Stock stock = new Stock();
+
+        Integer before = stock.getPromotionStock().get(Products.COKE);
+        stock.minusQuantity("콜라", 1);
+        Integer after = stock.getPromotionStock().get(Products.COKE);
+
+        Assertions.assertThat(before - after).isEqualTo(1);
+
+    }
+}
