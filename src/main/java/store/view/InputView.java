55a7adff72d32ee1754d3ae6078c94f5b47a09b0
himinqq <alsrud0966@naver.com>
2024-11-09 23:30:05 +0900

    feat(OutputView): 재고 부족으로 인한 일부 정가 결제 여부 입력받는 기능 구현

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -6,6 +6,7 @@ import store.ErrorMessage;
 public class InputView {
     private static final String purchaseStockMessage = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
     private static final String notifyPromotionMessage = "현재 {상품명}은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
+    private static final String notifyOutOfStockNoPromotionMessage = "현재 {상품명} {수량}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)";
 
     private final Supplier<String> reader;
 
@@ -23,6 +24,13 @@ public class InputView {
         validateYesOrNoAnswer(answer);
         return answer.equals("Y");
     }
+    public boolean requestApplyOutOfStockNoPromotion(){
+        System.out.println(notifyOutOfStockNoPromotionMessage);
+        String answer = reader.get();
+        validateYesOrNoAnswer(answer);
+        return answer.equals("Y");
+    }
+
     private void validateYesOrNoAnswer(String answer){
         if(!answer.equals("Y") && !answer.equals("N")){
             throw new IllegalArgumentException(ErrorMessage.YES_OR_NO_ANSWER_ERROR.getMessage());
