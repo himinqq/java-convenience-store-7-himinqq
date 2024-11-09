7d556f309bf8b5c33af72619aa0b7f9096c95f40
himinqq <alsrud0966@naver.com>
2024-11-09 23:30:51 +0900

    feat(OutputView): 멤버십 할인 여부 입력받는 기능 구현

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -7,6 +7,7 @@ public class InputView {
     private static final String purchaseStockMessage = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
     private static final String notifyPromotionMessage = "현재 {상품명}은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
     private static final String notifyOutOfStockNoPromotionMessage = "현재 {상품명} {수량}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)";
+    private static final String membershipApplyMessage = "멤버십 할인을 받으시겠습니까? (Y/N)";
 
     private final Supplier<String> reader;
 
@@ -26,6 +27,12 @@ public class InputView {
     }
     public boolean requestApplyOutOfStockNoPromotion(){
         System.out.println(notifyOutOfStockNoPromotionMessage);
+        String answer = reader.get();
+        validateYesOrNoAnswer(answer);
+        return answer.equals("Y");
+    }
+    public boolean requestApplyMembershipDiscount(){
+        System.out.println(membershipApplyMessage);
         String answer = reader.get();
         validateYesOrNoAnswer(answer);
         return answer.equals("Y");
